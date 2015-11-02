0011065d83493392bc4d9450dba91460b7dc0db0
muchinda <mukaratian@gmail.com>
2015-11-02 14:09:06 +0200

    added sill in the top comment block
    Signed-off-by: muchinda <mukaratian@gmail.com>

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <!--
+----------			=========				--  			  --		
+--	 -----			--		--				--	--			  -- 	sdfsdfsdf
+  -  -          	--		  --			--	   --		  --		
+  -  -            	- =========	--		 	--		 --		  -- 	
+  -  -             	--			--			--		  --	  -- 	
+  -  -              --			--			--			--	  --
+--   -----			--			--			--			  --  --
+----------			--			--			--			   -- --
+--->
+
+		<meta charset="utf-8">
+
+		<title>Ian Richard Mukarati....</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300|Arimo|Pacifico' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/style.css">
+
+	
+	</head>
+
+	<body class="animated bounceInDown">
+		<div class="avatar">
+			<img src="muchindawacho.jpg" alt="richierich" width="193">
+		</div>
+
+		<div class="content">
+			<h1 class="title">RICHARD IAN<br>
+			MUKARATI</h1>
+
+			<p>
+				MIND OF CODE </p>
+		  <p>EYE OF A DESIGNER</p>
+
+			<ul class="social-icons">
+			  <li><a class="twitter" href="http://www.twitter.com/mukaratian" title="Twitter">Twitter</a></li>
+				<li><a class="facebook" href="http://www.facebook.com/ianmukarati" title="Facebook">Facebook</a></li>
+				<li><a class="googleplus" href="#" title="wwww.google.com/ianmukarati">Google+</a></li>
+				
+				
+			</ul>
+
+			<p>
+
+				
+				<a href="Richard Ian Mukarati Resume.docx">Download Resume</a>
+			</p>
+	<h3>Want to send me an email?</h3>
+    
+<p> <a href="mailto:ianmukarati@icloud.com">ianmukarati@icloud.com</a>.</p>
+
+		
+		</div>
+	</body>
+</html>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55368730-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
